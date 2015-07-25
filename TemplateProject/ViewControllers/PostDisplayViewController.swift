2d702f72032664c457fe8fe64888393058f48ccd
Caitlyn Chen <caitlynachen@gmail.com>
2015-07-24 23:08:04 -0700

    autocompletion for country :)

--- TemplateProject/ViewControllers/PostDisplayViewController.swift
+++ TemplateProject/ViewControllers/PostDisplayViewController.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 import Parse
+import MapKit
 import Bond
 
-class PostDisplayViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+class PostDisplayViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate, NSURLConnectionDataDelegate{
     
+    @IBOutlet weak var autocompleteTextfield: AutoCompleteTextField!
     
-    @IBOutlet weak var picker: UIPickerView!
+    private var responseData:NSMutableData?
 
+    private var connection:NSURLConnection?
+    
+    private let googleMapsKey = "AIzaSyD8-OfZ21X2QLS1xLzu1CLCfPVmGtch7lo"
+    private let baseURLString = "https://maps.googleapis.com/maps/api/place/autocomplete/json"
+    
     var photoTakingHelper: PhotoTakingHelper?
     
-    @IBOutlet weak var countryTextField: UITextField!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView?
     @IBOutlet weak var instructionTableView: UITableView!
@@ -56,7 +62,7 @@ class PostDisplayViewController: UIViewController, UINavigationControllerDelegat
 //            var data = annotation?.image.getData()
 //            image = UIImage(data: data!)
 //            imageView?.image = image
-            countryTextField.text = annotation?.country
+            autocompleteTextfield.text = annotation?.country
             
             ing = annotation?.ingredients
             ins = annotation?.instructions
@@ -220,37 +226,18 @@ class PostDisplayViewController: UIViewController, UINavigationControllerDelegat
         }
         
     }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-        return pickerData.count
-    }
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-        return pickerData[row]
-    }
-
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
-        countryTextField.text = pickerData[row]
-        self.view.endEditing(true)
-    }
+ 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
      
-        
+        configureTextField()
+        handleTextFieldInterfaces()
         
         //picker = UIPickerView()
         
-        picker.delegate = self
-        picker.dataSource = self
-        
-        countryTextField.inputView = picker
-        countryTextField.text = pickerData[0]
+    
         
         descriptionText.delegate = self
         placeholderLabel = UILabel()
@@ -279,6 +266,81 @@ class PostDisplayViewController: UIViewController, UINavigationControllerDelegat
         // Do any additional setup after loading the view.
     }
     
+    private func configureTextField(){
+        autocompleteTextfield.autoCompleteTextColor = UIColor(red: 128.0/255.0, green: 128.0/255.0, blue: 128.0/255.0, alpha: 1.0)
+        autocompleteTextfield.autoCompleteTextFont = UIFont(name: "HelveticaNeue-Light", size: 12.0)
+        autocompleteTextfield.autoCompleteCellHeight = 35.0
+        autocompleteTextfield.maximumAutoCompleteCount = 20
+        autocompleteTextfield.hidesWhenSelected = true
+        autocompleteTextfield.hidesWhenEmpty = true
+        autocompleteTextfield.enableAttributedText = true
+        var attributes = [String:AnyObject]()
+        attributes[NSForegroundColorAttributeName] = UIColor.blackColor()
+        attributes[NSFontAttributeName] = UIFont(name: "HelveticaNeue-Bold", size: 12.0)
+        autocompleteTextfield.autoCompleteAttributes = attributes
+    }
+    
+    private func handleTextFieldInterfaces(){
+        autocompleteTextfield.onTextChange = {[weak self] text in
+            if !text.isEmpty{
+                if self!.connection != nil{
+                    self!.connection!.cancel()
+                    self!.connection = nil
+                }
+                let urlString = "\(self!.baseURLString)?key=\(self!.googleMapsKey)&input=\(text)"
+                let url = NSURL(string: urlString.stringByAddingPercentEscapesUsingEncoding(NSASCIIStringEncoding)!)
+                if url != nil{
+                    let urlRequest = NSURLRequest(URL: url!)
+                    self!.connection = NSURLConnection(request: urlRequest, delegate: self)
+                }
+            }
+        }
+        
+        autocompleteTextfield.onSelect = {[weak self] text, indexpath in
+            Location.geocodeAddressString(text, completion: { (placemark, error) -> Void in
+                if placemark != nil{
+                    let coordinate = placemark!.location.coordinate
+                    
+                    self!.autocompleteTextfield.text = text
+                }
+            })
+        }
+    }
+    
+    
+    //MARK: NSURLConnectionDelegate
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse) {
+        responseData = NSMutableData()
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        responseData?.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection) {
+        if responseData != nil{
+            var error:NSError?
+            if let result = NSJSONSerialization.JSONObjectWithData(responseData!, options: nil, error: &error) as? NSDictionary{
+                let status = result["status"] as? String
+                if status == "OK"{
+                    if let predictions = result["predictions"] as? NSArray{
+                        var locations = [String]()
+                        for dict in predictions as! [NSDictionary]{
+                            locations.append(dict["description"] as! String)
+                        }
+                        self.autocompleteTextfield.autoCompleteStrings = locations
+                    }
+                }
+                else{
+                    self.autocompleteTextfield.autoCompleteStrings = nil
+                }
+            }
+        }
+    }
+    
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        println("Error: \(error.localizedDescription)")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -296,7 +358,7 @@ class PostDisplayViewController: UIViewController, UINavigationControllerDelegat
         //change parse info
         annotation?.post.RecipeTitle = titleTextField.text
         annotation?.post.caption = descriptionText.text
-        annotation?.post.country = countryTextField.text
+        annotation?.post.country = autocompleteTextfield.text
         annotation?.post.Ingredients = ingredientsArray
         annotation?.post.Instructions = instructionsArray
         
@@ -312,7 +374,7 @@ class PostDisplayViewController: UIViewController, UINavigationControllerDelegat
         
         post.caption = descriptionText.text
         post.RecipeTitle = titleTextField.text
-        post.country = countryTextField.text
+        post.country = autocompleteTextfield.text
         post.location = toLoc
         post.Ingredients = self.ingredientsArray
         post.Instructions = self.instructionsArray
