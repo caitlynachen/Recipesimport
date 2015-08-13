7aa1d2c3e8f5c84c951f25e743a09e9a145f8542
Caitlyn Chen <caitlynachen@gmail.com>
2015-08-13 11:58:44 -0700

    h

--- TemplateProject/AppDelegate.swift
+++ TemplateProject/AppDelegate.swift
@@ -45,21 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     self.pushNotificationController = PushNotificationController()
     
-    // Register for Push Notitications, if running iOS 8
-    if application.respondsToSelector("registerUserNotificationSettings:") {
-        
-        let types:UIUserNotificationType = (.Alert | .Badge | .Sound)
-        let settings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes: types, categories: nil)
-        
-        application.registerUserNotificationSettings(settings)
-        application.registerForRemoteNotifications()
-        
-    } else {
-        // Register for Push Notifications before iOS 8
-        application.registerForRemoteNotificationTypes(.Alert | .Badge | .Sound)
-    }
-    
-    
         // 3
         // if we have a user, set the TabBarController to be the initial View Controller
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -81,25 +66,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
   }
     
-    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-        println("didRegisterForRemoteNotificationsWithDeviceToken")
-        
-        let currentInstallation = PFInstallation.currentInstallation()
-        
-        currentInstallation.setDeviceTokenFromData(deviceToken)
-        currentInstallation.saveInBackgroundWithBlock { (succeeded, e) -> Void in
-            //code
-        }
-    }
-    
-    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-        println("failed to register for remote notifications:  (error)")
-    }
-    
-    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        println("didReceiveRemoteNotification")
-        PFPush.handlePush(userInfo)
-    }
 
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
